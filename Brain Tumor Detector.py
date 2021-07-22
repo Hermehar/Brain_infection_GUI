7a400acba4465d4e6b77c900b45e75f6f3011bfa
Hermehar <63737394+Hermehar@users.noreply.github.com>
2021-07-22 19:15:15 +0530

    Update Brain Tumor Detector.py

--- Brain Tumor Detector.py
+++ Brain Tumor Detector.py
@@ -1,24 +1,25 @@
-"""This program helps the user to find out the brain tumor from the brain MRI.
---- This program takes the image from the user and identifies the tumor in it.
---- Tumor is represented in red color.
---- Firstly, the original image is shown,
---- Secondly, the image will be shown, which will mark the sufficiently bright parts of the image 
-    with red color indiacting the tumor and rest of the part will be colored with green color indicating 
-    that this region does not have any tumor.
---- Further, the image of tumor in the brain will be shown.
---- Lastly, the report of the patient will be sent on his whatsapp. If no traces of infection found, then it will say No traces found, else it will say traces of infections found. 
+#-----------------------BRAIN INFECTION DETECTOR----------------------------------                    
+"""
+-- This program helps the user to find out the brain infection from the brain MRI.
+-- This program takes the image from the user and identifies the infection in it.
+-- Infection is represented in red color.
+-- Firstly, the original image is shown,
+-- Secondly, the image will be shown, which will mark the sufficiently bright parts of the image  
+with red color indiacting the infection and rest of the part will be colored with green color indicating 
+that this region does not have any infection.
+-- Further, the image of infection in the brain will be shown.
+-- Lastly, the report of the patient will be sent on his whatsapp. If no traces of infection found, 
+then it will say No traces found, else it will say traces of infections found. 
 """
 
 from tkinter import *                                       #importing files
 from tkinter import filedialog
-from PIL import Image, ImageTk, ImageSequence  
+from PIL import Image, ImageTk 
 import PIL
 from simpleimage import SimpleImage
 import pywhatkit
 
-
 BRIGHT_THRESHOLD = 200                                      #bright threshold to find sufficiently brighter pixels
-
 
 root = Tk()                                                 #giving title and heading
 root.title("Brain Infection Detector")
@@ -27,10 +28,8 @@ NAME_ENTRY_FIELD = Entry(root, width = 50, fg = "red")      #storing values of t
 AGE_ENTRY_FIELD = Entry(root, width = 50, fg = "red")
 MOBILE_ENTRY_FIELD = Entry(root, width = 50, fg = "red")
 
-
 def get_avg(pixel):                                     #getting the average of pixels
         return (pixel.red + pixel.green + pixel.blue) // 3
-
 
 def trim_filename(original_filename, trim_size):        #cropping the image
         new_width = original_filename.width - 2 * trim_size
@@ -44,8 +43,7 @@ def trim_filename(original_filename, trim_size):        #cropping the image
                 trimmed_filename.set_pixel(x,y,orig_pixel)
         return trimmed_filename
 
-
-def find_tumor(filename):                              #sufficiently brighter pixels are colored red and rest are colored green
+def find_infection(filename):                              #sufficiently brighter pixels are colored red and rest are colored green
         for pixel in filename:
             pixel_avg = get_avg(pixel)
             if pixel.red >= BRIGHT_THRESHOLD:
@@ -53,15 +51,13 @@ def find_tumor(filename):                              #sufficiently brighter pi
                 pixel.green = 0
                 pixel.blue = 0
             else:
-                pixel.green = 0                   #displaying green color 
+                pixel.green = 0                  #displaying green color 
                 pixel.red = 0
                 pixel.blue = 0       
         return filename
 
-
 def open_file():                                        #opening a file
     root.filename = filedialog.askopenfilename(initialdir = r"C:\Users\Lenovo\Desktop\Hermehar\python", title = "Select File to open", filetypes = (("Jpeg images","*.jpg"),("Png images","*.png"),("All files","*.*")))
-
 
 def layout():                                           #entry fields
     name_entry = Label(root, text = "Enter Your Name").grid(row = 3, column = 1)                           
@@ -77,37 +73,39 @@ def layout():                                           #entry fields
 
     open_filename_button = Button(root, text = "Select File", command = open_file, fg = "#33FFCA", bg = "black").grid(row = 7, column = 2)
 
-
-def cancel_button():                                    #cancel nutton to quit the program
+def cancel_button():                                        #cancel nutton to quit the program
     quit()
 
-
 def submit_button():                                    #defining submit button
+
     
     def main():                                         #displaying the images and sending the report
-        open_layout = layout()        
-        filename = root.filename
-        filename = SimpleImage(filename)
+        open_layout = layout()                          #calling the layout function to give layout of the program
+        filename = root.filename                        #opening directory for selecting a file
+        filename = SimpleImage(filename)                #selecting a file
         filename.show()                                    #display original image
-        find_tumor_filename = find_tumor(filename)          
-        find_tumor_filename.show()                        #display image of brain with tumor 
+        find_infection_filename = find_infection(filename)          
+        find_infection_filename.show()                        #display image of brain with infection 
         trimmed_filename = trim_filename(filename, 40)
-        find_tumor_filename = find_tumor(trimmed_filename)
-        find_tumor_filename.show()                        #display part of brain with tumor  
-        find_tumor_filename.pil_image.save('tumor_output.jpg')
+        find_infection_filename = find_infection(trimmed_filename)
+        find_infection_filename.show()                        #display part of brain with infection  
+        find_infection_filename.pil_image.save('infection_output.jpg')
         
         def send_report():                                      #storing values of entryfields in a variable and sending message on user's whatsapp            
             name = NAME_ENTRY_FIELD.get()                       #storing values of entryfields in a variable
             age = AGE_ENTRY_FIELD.get()                        
             mobile = MOBILE_ENTRY_FIELD.get()
-            root.choose_filename = filedialog.askopenfilename(initialdir = r"C:\Users\Lenovo\Desktop\Hermehar\python", title = "Select File to open", filetypes = (("Jpeg images","*.jpg"),("Png images","*.png"),("All files","*.*"))) 
-            output_filename = root.choose_filename
-            output_filename = SimpleImage(output_filename)
-            for pixel in filename:
-                if pixel.green == 0:
-                    pywhatkit.sendwhatmsg_instantly(f'+919058422269', f'This is the report of {name}, age: {age} . Traces of infection have been found in the sample.The red portion in the image indicates the infection in the brain.')
+            root.choose_filename = filedialog.askopenfilename(initialdir = r"C:\Users\Lenovo\Desktop\Hermehar\python", title = "Select File to open", filetypes = (("Jpeg images","*.jpg"),("Png images","*.png"),("All files","*.*"))) #given an address to the directory to open for selecting a file
+            output_filename = root.choose_filename              #opening directory for selecting a file
+            output_filename = SimpleImage(output_filename)      #selecting a file
+            for pixel in output_filename:
+                pixel_avg = (pixel.red + pixel.green + pixel.blue) // 3
+                if pixel.red == pixel_avg * 4:
+                    pywhatkit.sendwhatmsg_instantly(f'{mobile}', f'This is the report of {name}, age: {age}. Traces of infection have been found in the sample.The red portion in the image indicates the infection in the brain.')
+                    break
                 else:
-                    pywhatkit.sendwhatmsg_instantly(f'+919058422269', f'This is the report of {name}, age: {age} . No Traces of infection have been found in the sample.')
+                    pywhatkit.sendwhatmsg_instantly(f'{mobile}', f'This is the report of {name}, age: {age}. No Traces of infection have been found in the sample.')
+                    break
 
             Report = Label(root, text = f'Report of {name} has been sent to {mobile}', fg = "purple").grid(row = 9, column = 2)
             Thanks = Label(root, text = "Thanks for using the software", fg = "purple").grid(row = 11, column = 2) 
@@ -117,11 +115,8 @@ def submit_button():                                    #defining submit button
 
     if __name__ == '__main__':
         main()
-
-
-#adding two buttons                                                       
+                                                               #adding three buttons
 submit_button = Button(root, text = "Open", command = submit_button, fg = "#33FFCA", bg = "black").grid(row = 12, column = 1)
 cancel_button = Button(root, text =  "Close", command = cancel_button, fg = "#33FFCA", bg = "black").grid(row = 12, column = 2)
 
-
-root.mainloop()                                         #ending the document
+root.mainloop()          
